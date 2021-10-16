604ad51d7d815ea216e5a219cf0d1bf116e8677d
Gianlucca Puglia <gianlucca.puglia@procempa.com.br>
2021-10-16 15:18:21 -0300

    fixing controller test and adding integration test

--- src/test/groovy/com/yieldstreet/home/challenge/controller/UserAccreditationControllerSpockTests.groovy
+++ src/test/groovy/com/yieldstreet/home/challenge/controller/UserAccreditationControllerSpockTests.groovy
@@ -10,51 +10,89 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 
 import org.spockframework.spring.SpringBean
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
+
 import com.yieldstreet.home.challenge.controller.UserAccreditationController
 import com.yieldstreet.home.challenge.model.AccreditationProof
 import com.yieldstreet.home.challenge.model.Document
+import com.yieldstreet.home.challenge.util.FileManagementUtils
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
+import org.springframework.test.context.ContextConfiguration
 
-@WebMvcTest(UserAccreditationController)
-@AutoConfigureMockMvc
+//@WebMvcTest(UserAccreditationController)
+@ContextConfiguration 
+@SpringBootTest
+@AutoConfigureMockMvc 
 class UserAccreditationControllerSpockTests extends Specification {
 
 	
 	@Autowired
 	MockMvc mockMvc
 	
-	@SpringBean
-	UserAccreditationService userAccreditationService = Mock()
+	@Autowired
+	DocumentService documentService
 	
+	def doc1,doc2
 	
-	def "should save the documents and receiving a POST"() {
+	def setup() {
+		doc1 = [ name: "2019.jpg", mime_type: "image/jpg", user_id: "g8NlYJnk7zK9BlB1J2Ebjs0AkhCTpE1V", content:"ICAiQC8qIjogWyJzcmMvKiJdCiAgICB9CiAgfQp9Cg=="]
+		doc2 =	[ name: "2018.pdf",mime_type: "application/pdf",  user_id: "g8NlYJnk7zK9BlB1J2Ebjs0AkhCTpE1V", content:"91cy1wcm9taXNlICJeMi4wLjUiCiAgICB0b3Bvc29ydCAiXjIuMC4yIgoi=="]
+	}
+	
+	//TODO move this payload to a file
+	def "should save the documents after receiving a POST"() {
 		given: "a well formed request containing 2 documents"
-		def requestBody = ''' {
-						 "user_id": "g8NlYJnk7zK9BlB1J2Ebjs0AkhCTpE1V",
+		def requestBody = """ {
+						 "user_id": "$doc1.user_id",
 						 "payload": {
 							 "accreditation_type": "BY_INCOME",
 							 "documents": [{
-								 "name": "2018.pdf",
-								 "mime_type": "application/pdf",
-								 "content": "ICAiQC8qIjogWyJzcmMvKiJdCiAgICB9CiAgfQp9Cg=="
+								 "name": "$doc1.name",
+								 "mime_type": "$doc1.mime_type",
+								 "content": "$doc1.content"
 								 },{
-								 "name": "2019.jpg",
-								 "mime_type": "image/jpeg",
-								 "content": "91cy1wcm9taXNlICJeMi4wLjUiCiAgICB0b3Bvc29ydCAiXjIuMC4yIgoi=="
+								 "name": "$doc2.name",
+								 "mime_type": "$doc2.mime_type",
+								 "content": "$doc2.content"
 								 }
 							 ]
 						 }
-						}'''
+						}"""
 		when:
 			def response = mockMvc.perform(post('/user/accreditation').contentType("application/json").content(requestBody))
 								
 		then:
-			response //response.andExpect(status().isOk())
+			response.andExpect(status().isOk())
+	}
+	
+	//TODO move this payload to a file
+	def "should return bad request when receive malformed input"() {
+		given: "a malformed input"
+		def malformedRequest = """ {
+						 "user_id": "$doc1.user_id",
+						 "payload": {
+							 "accreditation_type": "OTHER",
+							 "documents": [{
+								 "name": "$doc1.name"
+								 }
+							 ]
+						 }
+						}"""
+		when:
+			def response = mockMvc.perform(post('/user/accreditation').contentType("application/json").content(malformedRequest))
+								
+		then:
+			response.andExpect(status().isBadRequest())
+	}
+	
+	def cleanup() {
+		FileManagementUtils.deleteFile(documentService.getBasepath() + "\\$doc1.user_id\\$doc1.name" )
+		FileManagementUtils.deleteFile(documentService.getBasepath() + "\\$doc2.user_id\\$doc2.name" )
+		
 	}
 	
 }
